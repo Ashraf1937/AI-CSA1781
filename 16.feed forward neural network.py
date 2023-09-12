dabd552ad000fea65f5ce372538f8d4ed9275dc6
Ashraf1937 <113973724+Ashraf1937@users.noreply.github.com>
2023-09-12 06:39:57 +0530

    Update 16.feed forward neural network.py

--- 16.feed forward neural network.py
+++ 16.feed forward neural network.py
@@ -1,19 +1,39 @@
 import numpy as np
 
+x_all = np.array(([2, 9], [1, 5], [3, 6], [5, 10]), dtype=float) # input data
+y = np.array(([92], [86], [89]), dtype=float) # output
 
-def relu(n):
-    if n<0:
-        return 0
-    else:
-        return n
+x_all = x_all/np.amax(x_all, axis=0)
+y = y/100 
 
+X = np.split(x_all, [3])[0] 
+x_predicted = np.split(x_all, [3])[1]
 
-inp=np.array([[-1,2],[2,2],[3,3]])
-weights=[np.array([3,3]),np.array([1,5]),np.array([3,3]),np.array([1,5]),np.array([2,-1])]
-for x in inp :
-    node0=relu((x*weights[0]).sum())
-    node1=relu((x*weights[1]).sum())
-    node2=relu(([node0,node1]*weights[2]).sum())
-    node3=relu(([node0,node1]*weights[3]).sum())
-    op=relu(([node2,node3]*weights[4]).sum())
-    print(x,op)
+class neural_network(object):
+  def __init__(self):
+    #parameters
+    self.inputSize = 2
+    self.outputSize = 1
+    self.hiddenSize = 3
+
+    #weights
+    self.W1 = np.random.randn(self.inputSize, self.hiddenSize) 
+    self.W2 = np.random.randn(self.hiddenSize, self.outputSize)
+
+  def forward(self, X):
+    self.z = np.dot(X, self.W1) 
+    self.z2 = self.sigmoid(self.z) 
+    self.z3 = np.dot(self.z2, self.W2) 
+    o = self.sigmoid(self.z3) # final activation function
+    return o
+
+  def sigmoid(self, s):
+    # activation function
+    return 1/(1+np.exp(-s))
+
+nn = neural_network()
+
+o = nn.forward(X)
+
+print ("Predicted Output: \n" + str(o))
+print ("Actual Output: \n" + str(y))
